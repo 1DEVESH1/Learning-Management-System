1a77d80c457c9a2933eea89477acb72fcd39aac3
Devesh <136490739+1DEVESH1@users.noreply.github.com>
2025-10-11 21:42:40 +0530

    changed localhost links to deployed links

--- server/controllers/coursePurchase.controller.js
+++ server/controllers/coursePurchase.controller.js
@@ -39,8 +39,8 @@ export const createCheckoutSession = async (req, res) => {
         },
       ],
       mode: "payment",
-      success_url: `http://localhost:5173/course-progress/${courseId}`, // once payment successful redirect to course progress page
-      cancel_url: `http://localhost:5173/${courseId}`,
+      success_url: `https://learning-management-system-o8um.onrender.com/course-progress/${courseId}`, // once payment successful redirect to course progress page
+      cancel_url: `https://learning-management-system-o8um.onrender.com/${courseId}`,
       metadata: {
         courseId: courseId,
         userId: userId,
--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://learning-management-system-o8um.onrender.com",
     credentials: true,
   })
 );
